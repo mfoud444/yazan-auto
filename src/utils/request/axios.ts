acaffe95029cfb319996ca8acf35a35e09bf55e6
Mohammed Foud <mfoud444@gmail.com>
2024-10-12 01:57:09 +0300

    all

--- src/utils/request/axios.ts
+++ src/utils/request/axios.ts
@@ -2,8 +2,8 @@ import axios, { type AxiosResponse } from 'axios'
 import { useAuthStore } from '@/store'
 import applyCaseMiddleware from 'axios-case-converter';
 // export const baseURL = "import.meta.env.VITE_GLOB_API_URL"; 
-export const baseURL = "http://0.0.0.0:8000"
-// export const baseURL = "https://furious-trina-sarr-3ce5a089.koyeb.app"
+// export const baseURL = "http://0.0.0.0:8000"
+export const baseURL = "https://furious-trina-sarr-3ce5a089.koyeb.app"
 
 const service = applyCaseMiddleware(axios.create({
   baseURL:baseURL,
